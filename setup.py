c172f66501fcdc80c1517d21e6cbf4b7375af513
KIM GYU SUNG <support@c0met.kr>
2024-12-30 04:05:59 +0900

    COSQL -> CoSQL

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'COSQL',
-    version = '0.2.0',
-    description = 'SQLite3 wrapper for asyncio',
+    name = 'CoSQL',
+    version = '1.0.0',
+    description = 'Comet용 SQLite3 비동기 라이브러리',
 
     author = 'Comet',
-    url = 'https://github.com/cwmet/COSQL',
+    url = 'https://github.com/cwmet/CoSQL',
 
     packages = find_packages(exclude=[]),
-    python_requires = '>=3.12',
+    python_requires = '>=3.11',
     install_requires = ['asyncio'],
 )
